bcd47f0d92724ab40830ebabd766549eb2aae53c
pzyu <pangzhengyu@live.com>
2017-02-08 13:11:12 +0800

    Add new Contact class that has child classes: Phone, Email, Address

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,30 @@
+package seedu.addressbook.data.person;
+
+/**
+ *  Represents a Person's general contact details
+ */
+public class Contact {
+    public String value = "UNDEFINED";
+    protected boolean isPrivate;
+    
+    @Override
+    public String toString() {
+        return value;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
